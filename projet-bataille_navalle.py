f60f21cd92289e6ae48bd0b89d2e31e1e8a83fc2
Gascuena Leo <gascuenaleo@gmail.com>
2024-10-16 15:42:24 +0100

    début de fonction pour placer les bateaux

--- projet-bataille_navalle.py
+++ projet-bataille_navalle.py
@@ -1,6 +1,28 @@
+def stock_initial():
+    """
+    établi le stock de bateau par joueurs
+    sortie: un dictionnaire avec le nombre de bateau par taille
+    """
+    return {2: 1, 3: 2, 4: 1, 5: 1}
+
+def placer_bateau(grille, taille, orientation, coordonnee):
+    """
+    permet de placer un bateau
+    """
+    if #en gros faudrait que si le bateau n'est pas disponible il est impossible de le placer
+    #renvoie une erreur
+    if orientation == True:
+        for i in grille:
+
+
+
+
 def create_game():
-    stock_p1 = initialiser_stock()
-    stock_p2 = initialiser_stock()
+    """
+    permet de créer la partie en établissant une grille par joueurs
+    """
+    stock_p1 = stock_initial()
+    stock_p2 = stock_initial()
     tableau = []
     lignes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
